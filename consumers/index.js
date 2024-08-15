98ea9e66109fd023640344289dd545cefa530d22
Tuan.Nguyen <>
2024-08-15 15:25:31 +0700

    fix docker

--- consumers/index.js
+++ consumers/index.js
@@ -3,7 +3,7 @@ import { DaprServer } from "@dapr/dapr";
 const daprHost = process.env.DAPR_HOST || "http://localhost";
 const daprPort = process.env.DAPR_HTTP_PORT || "3501";
 const serverHost = process.env.SERVER_HOST || "127.0.0.1";
-const serverPort = process.env.APP_PORT || "5002";
+const serverPort = process.env.APP_PORT || "500002";
 const pubSubName = "userpubsub";
 const pubSubTopic = "createUser";
 
